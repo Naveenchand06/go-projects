6025a8608c74c7e78c3cbbcfd835320d739f1321
Naveenchand06 <naveenchand0606@gmail.com>
2023-12-17 20:43:44 +0530

    Go Mongo

--- go-mongo/controllers/user-controller.go
+++ go-mongo/controllers/user-controller.go
@@ -1,1 +1,89 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Naveenchand06/go-projects/go-mongo/models"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+
+type UserController struct {
+	Session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (c *UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	oid := bson.ObjectIdHex(id)
+	u := models.User{}
+	err := c.Session.DB("mongo-golang").C("users").FindId(oid).One(&u)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println("Marshal error: ", err)
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (c *UserController) CreateUser(w http.ResponseWriter, r*http.Request, p httprouter.Params) {
+	u := models.User{}
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return;
+	}
+	u.ID = bson.NewObjectId()
+	err = c.Session.DB("mongo-go").C("users").Insert(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+
+func (c *UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	err := c.Session.DB("mogo-go").C("users").RemoveId(oid)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "User Deleted", oid, "\n")
+
+}
--- go-mongo/main.go
+++ go-mongo/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/Naveenchand06/go-projects/go-mongo/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+
+func main() {
+	router := httprouter.New()
+	controller := controllers.NewUserController(getSession())
+	router.GET("/user/:id", controller.GetUser)
+	router.POST("/user", controller.CreateUser)
+	router.DELETE("/user/:id", controller.DeleteUser)
+
+	// * Listen for incoming requests
+	http.ListenAndServe(":5010", router)
+
+}
+
+
+func getSession() *mgo.Session {
+	session, err :=  mgo.Dial("mongodb://myuser:mypass@localhost:27017/mongo-go")
+	if err != nil {
+		panic(err)
+	}
+	return session
+}
